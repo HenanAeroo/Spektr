05c02093ebb4666e5e8cc376e280ccc9d1cd9b50
nhenan <henan.noel@ynov.com>
2026-06-08 11:16:58 +0200

    test(auth): tests localRegister and localLogin

--- apps/api/src/auth/auth.service.spec.ts
+++ apps/api/src/auth/auth.service.spec.ts
@@ -8,6 +8,7 @@ import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
 import { MailService } from '../mail/mail.service';
 import bcrypt from 'bcrypt';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
 
 const mockPrisma = {
   user: {
@@ -129,6 +130,75 @@ describe('AuthService', () => {
         id: 1,
         email: 'test@gmail.com',
       });
+    });
+  });
+
+  describe('localRegister', () => {
+    it('throw a ConflictEception if the email already used', async () => {
+      mockUsersService.findOne.mockResolvedValue({ id: 1 });
+
+      await expect(
+        service.localRegister({
+          email: 'test@gmail.com',
+          password: 'Test1234!',
+          first_name: 'John',
+          last_name: 'Doe',
+        }),
+      ).rejects.toThrow(ConflictException);
+    });
+
+    it('create the user and send a confirmation email', async () => {
+      mockUsersService.findOne.mockResolvedValue(null);
+      (bcrypt.hash as jest.Mock).mockResolvedValue('hashed_pw');
+      mockPrisma.user.create.mockResolvedValue({
+        id: 1,
+        email: 'test@gmail.com',
+      });
+      mockPrisma.user.update.mockResolvedValue({
+        id: 1,
+        email: 'test@gmail.com',
+        first_name: 'Test',
+      });
+
+      const result = await service.localRegister({
+        email: 'test@gmail.com',
+        password: 'Test1234!',
+        first_name: 'Test',
+        last_name: 'User',
+      });
+
+      expect(bcrypt.hash).toHaveBeenCalledWith('Test1234!', 12);
+      expect(mockPrisma.user.create).toHaveBeenCalled();
+      expect(mockPrisma.user.update).toHaveBeenCalled();
+      expect(mockMailService.send).toHaveBeenCalledTimes(1);
+      expect(result).toBe('Un email de confirmation a été envoyé');
+    });
+  });
+
+  describe('localLogin', () => {
+    it('throw UnauthorizedException if the email is not verified', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue({
+        id: 1,
+        emailVerified: false,
+      });
+
+      await expect(
+        service.localLogin({ sub: 1, role: 'STUDENT' }),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('returns accessToken and refreshToken if the email is verified', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue({
+        id: 1,
+        emailVerified: true,
+        role: 'STUDENT',
+      });
+      mockPrisma.refreshToken.create.mockResolvedValue({});
+
+      const result = await service.localLogin({ sub: 1, role: 'STUDENT' });
+
+      expect(result).toHaveProperty('accessToken');
+      expect(result).toHaveProperty('refreshToken');
     });
   });
 });
